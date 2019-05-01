11ac61755ea85aa41e65b011ab28324f0ee4e3be
andy <ajldrm007@gmail.com>
2019-04-30 22:50:23 -0400

    adding code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int a[] = {1,2,3};
+        int b[] = {3,2,1};
+        int alice = 0;
+        int bob = 0;
+
+        for(int i = 0; i <3; i++)
+        {
+            if(a[i]<b[i])
+            {bob++;}
+            if(a[i]>b[i])
+            {alice++;}
+        }
+        System.out.println("Bob's score: " + bob);
+        System.out.println("Alice's score: " + alice);
+
+
     }
 }
